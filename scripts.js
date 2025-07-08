0bde5503f6809ae847a9013230448bdae2478d61
jiins8 <117478765+jiins8@users.noreply.github.com>
2025-07-08 19:22:38 +0200

    proyecto aniwatch

---
+++ scripts.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Swiper
+  const aboutSlider = new Swiper(".about-slider", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+  });
+
+  // Inicializar EmailJS
+  emailjs.init("iIk3Z-iVtbuKOsxYs"); // reemplaza por tu clave pública
+
+  // Formulario de contacto
+  const form = document.getElementById("contact-form");
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("service_tjkx17x", "template_difsjbf", this)
+      .then(
+        () => {
+          alert("¡Mensaje enviado correctamente!");
+          form.reset();
+        },
+        (error) => {
+          console.error("Error al enviar:", error);
+          alert("Error al enviar el mensaje. Intenta más tarde.");
+        }
+      );
+  });
+});
